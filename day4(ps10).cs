c6c06caea3d60c1f74a8dce29532778c8566e96d
lakshmikiranb <111352316+lakshmikiranb@users.noreply.github.com>
2022-08-19 19:05:27 +0530

    Add files via upload

---
+++ day4(ps10).cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace day4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, rev = 0, rem;
+            Console.Write("Enter a Number: ");
+            num = int.Parse(Console.ReadLine());
+            while (num!=0)
+            {
+                rem = num%10;
+                rev = rev*10 + rem;
+                num/=10;
+            }
+            Console.Write("Reverse of the Number: " + rev);
+        }
+    }
