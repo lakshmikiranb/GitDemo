6b01d06131a216b00b573f507666d59abf3399ba
lakshmikiranb <111352316+lakshmikiranb@users.noreply.github.com>
2022-08-21 18:10:22 +0530

    Add files via upload

---
+++ day4(ps11).cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace day4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, sum = 0;
+
+            Console.Write("\n\n");
+            Console.Write("Find the number and sum of all integer between 100 and 200:\n");
+            Console.Write("-----------------------------------------------------------------------------");
+            Console.Write("\n\n");
+
+            Console.Write("Numbers between 100 and 200 : \n");
+            for (i=101; i<200; i++)
+            {
+            
+             sum+=i;
+            Console.Write("\n\nThe sum : {0} \n", sum);
+        }
+
+    }
