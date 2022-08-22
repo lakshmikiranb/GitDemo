6965bbe6aee32fce62ed1c6c3ee96bc1fca5ea5d
lakshmikiranb <111352316+lakshmikiranb@users.noreply.github.com>
2022-08-22 20:06:30 +0530

    Add files via upload

---
+++ day5(ps13).cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+{ 
+  internal class Program
+  {
+
+     static void Main(string[] args)
+     {
+
+        int[] array1 = { 1,3,5,200,22,2244,213,111 };
+
+        var big = from value in array1 where value > 100 select value;
+        Console.WriteLine("Numbers that are greater than 100 are  :");
+
+        foreach (var s in big)
+        {
+            Console.Write(s.ToString() + " ");
+        }
+        Console.Read();
+    }
+}
