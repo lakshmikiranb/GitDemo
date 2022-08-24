152fce2bdd20d7fd71dad726d108ea4bbc0c494e
lakshmikiranb <111352316+lakshmikiranb@users.noreply.github.com>
2022-08-24 18:15:39 +0530

    Add files via upload

---
+++ day6(ps3).cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+{
+class Employee
+{
+    public int eNum;
+    public string eName;
+    public int eSal;
+    public int eDep;
+
+    public Employee(int eNUM, string eName, int eSal, int eDep)
+    {
+        this.eNum=eNum;
+        this.eName = eName;
+        this.eSal = eSal;
+        this.eDep = eDep;
+    }
+
+    public static void PrintDataBase()
+    {
+
+    }
+
+    static void Main()
+    {
+        List<Employee> employees = new List<Employee>();
+        employees.Add(new Employee("PALI_e1", "Parvez Ali", "FOOTBALL", 1));
+        employees.Add(new Employee("AALI_e2", "Ashik Ali", "FOOTBALL", 1));
+        employees.Add(new Employee("AALI_e3", "Aftab Ali", "CHESS", 2));
+        employees.Add(new Employee("AALI_e4", "Arif Ali", "CRICKET", 3));
+
+        Menu.MenuPrinting();
+        Console.WriteLine("\nPlease Enter Your Choice");
+        string Choice = Console.ReadLine();
+        foreach (Employee e in employees)
+        {
+            Console.WriteLine(e);
+        }
+    }
+}
---
+++ day6(ps8).cs
@@ -0,0 +1,19 @@
+﻿using System;
+public class Program
+{
+    enum deptnames
+    {
+        Purchase = 1, 
+        Sales = 2, 
+        Training = 3,
+        Accounts = 4
+    };
+    public static void Main()
+    {
+        Console.WriteLine("The various stages in the deptnames are:\n");
+        foreach (string str in Enum.GetNames(typeof(deptnames)))
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
