5504c3f1523d4091b1dd5a8111dd03b8a30924b3
lakshmikiranb <111352316+lakshmikiranb@users.noreply.github.com>
2022-08-25 16:58:55 +0530

    Add files via upload

---
+++ day8(ps1)..cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace ConstructorDestructorEtcDemo
+{
+    public class Class
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream("demofile.txt", FileMode.Truncate, FileAccess.Write);
+            Console.WriteLine("Truncated");
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ day8(ps2).cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace ConstructorDestructorEtcDemo
+{
+    public class Class
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\91837\Downloads\newkiran";
+            string[] fnames = Directory.GetFiles(path);
+            foreach (var item in fnames)
+            {
+                Console.WriteLine(item);
+            }
+            //string[] dirs = Directory.GetDirectories(path);
+            //foreach (var item in dirs)
+            //{
+            //    Console.WriteLine(item);
+            //    string[] fnames=Directory.GetFiles(item);
+            //    foreach (var item1 in fnames)
+            //    {
+            //        Console.WriteLine("Files");
+            //        Console.WriteLine(item1);
+            //    }
+            //    Console.WriteLine("---------------");
+            //}
+            Console.ReadLine();
+        }
+    }
+}
