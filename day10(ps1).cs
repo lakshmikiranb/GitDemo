ddae155cbec22d7d86157dccea91576faa25d475
lakshmikiranb <111352316+lakshmikiranb@users.noreply.github.com>
2022-08-29 18:09:24 +0530

    Add files via upload

---
+++ day10(ps1).cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS_Day2
+{
+    internal class BInarySearch
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            int n, x, temp, t;
+            for(int i=0;i<10;i++)
+            {
+                Console.WriteLine("Enter element");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+        }
+    }
+}
