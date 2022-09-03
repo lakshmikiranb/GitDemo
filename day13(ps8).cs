d5cb2326ad69703a6cf4c6ab61ba4e2a40b1cc70
lakshmikiranb <111352316+lakshmikiranb@users.noreply.github.com>
2022-09-03 15:19:52 +0530

    Add files via upload

---
+++ day13(ps8).cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedAppDemo
+{
+    class Product
+    {
+        public static void Main(string[] args)
+        {
+            List<string> list = new List<string>()
+            {
+                "Apple", "Orange", "Kiwi", "Melon"
+            };
+            list.Add("Mango");
+            list.Add("Watermelon");
+
+            foreach (string item in list)
+            {
+                Console.WriteLine("Item: {0}", item);
+            }
+        }
+    }
+}
