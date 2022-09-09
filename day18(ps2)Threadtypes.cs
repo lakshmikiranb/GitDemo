8e2cc43de4e216413fb9ac7a49a776fc0c0b6c62
lakshmikiranb <111352316+lakshmikiranb@users.noreply.github.com>
2022-09-09 17:55:32 +0530

    Add files via upload

---
+++ day18(ps2)Threadtypes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ThreadingDemo
+{
+    class Threadtypes
+    {
+        static void MyThreadMethod()
+        {
+            Thread newThread =
+             new Thread(new ThreadStart(ThreadMethod));
+
+            Console.WriteLine("ThreadState: {0}", newThread.ThreadState);
+            newThread.Start();
+
+            // Wait for newThread to start and go to sleep.
+            Thread.Sleep(300);
+            Console.WriteLine("ThreadState: {0}", newThread.ThreadState);
+
+            // Wait for newThread to restart.
+            Thread.Sleep(1000);
+            Console.WriteLine("ThreadState: {0}", newThread.ThreadState);
+        }
+
+        static void ThreadMethod()
+        {
+            Thread.Sleep(1000);
+        }
+    }
+}
---
+++ day18(ps4)MonitorDemo.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.IO;
+
+namespace ThreadingDemo
+{
+    class MonitorDemo
+    {
+        static object l1 = new object();
+        static void PrintTable()
+        {
+
+            Thread.Sleep(800);
+            WriteToFile();
+
+        }
+
+        static void WriteToFile()
+        {
+            string threadname = Thread.CurrentThread.Name;
+            Console.WriteLine(threadname);
+            Monitor.Enter(l1);
+
+            try
+            {
+
+                using (FileStream fs = new FileStream("tables.txt", FileMode.Append, FileAccess.Write))
+                {
+                    StreamWriter wr = new StreamWriter(fs);
+                    wr.WriteLine(threadname);
+                    wr.Close();
+                    fs.Close();
+                }
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+
+
+                Monitor.Exit(l1);
+                Console.WriteLine("Releasing the thread " + threadname);
+
+            }
+
+
+
+
+        }
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 3; i++)
+            {
+                Thread thread = new Thread(PrintTable);
+                thread.Name = string.Concat("thread ", i);
+                thread.Start();
+
+            }
+            Console.Read();
+
+        }
+    }
+    class MutexDemo
+    {
+        static void Main(string[] args)
+        {
+            //imterprocess
+            using (Mutex mutex = new Mutex(false, "ThreadingDemo"))
+            {
+
+                if (!mutex.WaitOne(5000, false))
+                {
+                    Console.WriteLine("already running....");
+                    Console.ReadLine();
+                    return;
+
+                }
+                else
+                {
+                    Console.WriteLine("app is running");
+                    Console.ReadLine();
+                }
+
+            }
+
+        }
+    }
+}
+
