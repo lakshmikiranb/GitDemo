7ac544a0e989c1b914c1dda46368f2df7b104d65
lakshmikiranb <111352316+lakshmikiranb@users.noreply.github.com>
2022-09-13 16:26:00 +0530

    Add files via upload

---
+++ day20(ps C1).cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedAssembly
+{
+    public class Class1
+    {
+        protected void btnchange_Click(object sender, EventArgs e)
+        {
+            int changed;
+            string loginemailid = txtloginemailid.Text.Trim();
+            string oldpass = txtoldpassword.Text.Trim();
+            string newpass = txtnewpassword.Text.Trim();
+
+
+            if (loginemailid != null && oldpass != null)
+            {
+                changed = mydac.changeuserpassword(loginemailid, oldpass, newpass);
+                if (changed == 1)
+                {
+                    lblmsg.Text = "Your new Password Updated";
+
+                }
+            }
+            else
+            {
+                lblmsg.Text = "Updation Failed,Ensure Your Login Information Is Correct";
+            }
+        }
+    }
+}
---
+++ day20(ps C2).cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedAssembly
+{
+    public class Stringexercise
+    {
+        public static void Main()
+        {
+            string username, password;
+            int ctr = 0;
+            do
+            {
+                Console.Write("Input a username: ");
+                username = Console.ReadLine();
+
+                Console.Write("Input a password: ");
+                password = Console.ReadLine();
+
+                if (username != "abcd" || password != "1234")
+                    ctr++;
+                else
+                    ctr = 1;
+
+            }
+            while ((username != "admin" || password != "123456") && (ctr != 3));
+
+            if (ctr == 3)
+                Console.WriteLine("\nLogin attemp three or more times. Try later!");
+            else
+                Console.WriteLine("\nThe password entered successfully!");
+
+            Console.ReadLine();
+        }
+    }
+}
